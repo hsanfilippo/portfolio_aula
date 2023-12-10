adf6f09a364ced60e80d51d93586671cb04df15c
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2023-12-10 19:32:12 +0900

    aula_5

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/hsanfilippo.png" />
+    <Title>Henrique Sanfilippo</Title>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Title from '../../components/Title'
+
+const Sobre = () => (
+  <section>
+    <Title>Sobre</Title>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati facere
+      nihil iusto, quibusdam vitae illo quas, nulla, harum quia earum atque cum
+      neque? Quae reiciendis assumenda maiores animi expedita amet?
+    </p>
+  </section>
+)
+
+export default Sobre
