dc5f8c5ef0d7b79376a3ca093bc218a3ba826266
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2023-12-11 09:31:43 +0900

    adicionando tema dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import { ThemeProvider } from 'styled-components'
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import EstiloGlobal, { Container } from './styles'
 import Sobre from './containers/Sobre'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
     <>
-      <EstiloGlobal />
-      <Container>
-        <Sidebar />
-        <main>
-          <Sobre />
-          <Projetos />
-        </main>
-      </Container>
+      <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
+        <EstiloGlobal />
+        <Container>
+          <Sidebar trocaTema={trocaTema} />
+          <main>
+            <Sobre />
+            <Projetos />
+          </main>
+        </Container>
+      </ThemeProvider>
     </>
   )
 }
